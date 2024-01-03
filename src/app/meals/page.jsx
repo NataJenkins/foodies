5b42b1775e255489073697658a6a282be91b07d6
Natalia Macias <natajenkins@hotmail.com>
2024-01-03 17:56:45 -0600

    Meals page

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
 
 export default function MealsPage() {
-    return <div>MealsPage</div>;
+    return (
+        <header className={classes.header}>
+            <h1>
+                Delicious meals, created
+                <span className={classes.highlight}> by you</span>
+            </h1>
+            <p>
+                Choose yout favorite reciepie and cook it yourself. it is easy
+                and fun!{" "}
+            </p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">Share Your Favorite Recipie</Link>
+            </p>
+        </header>
+    );
 }
