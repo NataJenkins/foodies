e8ec8c45a2b15cbce77da19a8e055770066b7502
Natalia Macias <natajenkins@hotmail.com>
2024-01-03 19:17:33 -0600

    Loading and renderin Meal details with SLugs

--- src/app/meals/[mealsSlug]/page.tsx
+++ src/app/meals/[mealsSlug]/page.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { getMeal } from "../../../../lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-export default function MealsDetailPage() {
-    return <div>MealsDetailPage</div>;
+export default function MealsDetailPage({ params }) {
+    const meal = getMeal(params.mealsSlug);
+    meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by{" "}
+                        <a href={`mailto:${meal.creator_email}`}>
+                            {meal.creator}
+                        </a>
+                    </p>
+                    <div className={classes.summary}>{meal.summary}</div>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{ __html: meal.instructions }}
+                ></p>
+            </main>
+        </>
+    );
 }
