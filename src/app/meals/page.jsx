f4f014bdda5e323ccdbd4541b4c19127d415b143
Natalia Macias <natajenkins@hotmail.com>
2024-01-03 18:45:14 -0600

    Fetching data for meals grid

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
+import MealsGrid from "../../components/meals/meals-grid";
+import { getMeals } from "../../../lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
-        <header className={classes.header}>
-            <h1>
-                Delicious meals, created
-                <span className={classes.highlight}> by you</span>
-            </h1>
-            <p>
-                Choose yout favorite reciepie and cook it yourself. it is easy
-                and fun!{" "}
-            </p>
-            <p className={classes.cta}>
-                <Link href="/meals/share">Share Your Favorite Recipie</Link>
-            </p>
-        </header>
+        <>
+            {console.log(meals)}
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created
+                    <span className={classes.highlight}> by you</span>
+                </h1>
+                <p>
+                    Choose yout favorite reciepie and cook it yourself. it is
+                    easy and fun!{" "}
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipie</Link>
+                </p>
+            </header>
+            <main>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
     );
 }
--- src/components/meals/meals-grid.jsx
+++ src/components/meals/meals-grid.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./meals-grid.module.css";
 import MealItem from "./meal-item";
 
-export default function MealsGrid(meal) {
+export default function MealsGrid({ meals }) {
     return (
         <ul className={classes.meals}>
             {meals.map((meal) => (
